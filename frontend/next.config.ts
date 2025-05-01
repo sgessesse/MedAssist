471a79b0bc5ef6a833903845a3ac771bd4e9e915
“sgessesse” <sem_werede@yahoo.com>
2025-05-01 16:29:33 -0400

    rewrite http to https

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://medassist.us-east-2.elasticbeanstalk.com/api/:path*", // Note: HTTPS
+      },
+    ];
+  },
 };
 
 export default nextConfig;
